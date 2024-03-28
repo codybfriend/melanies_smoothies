475e9ddd6bc829b4c0107323a22ead3b22b65d64
codybfriend <165207101+codybfriend@users.noreply.github.com>
2024-03-28 08:49:37 -0400

    Update streamlit_app.py
    Add column Search ON

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 # Create variable and pull data from Fruit Options
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
